90bbbd3e51300be84a22225136d9dfd4a78140d6
Ilya Ovodov <b@ovdv.ru>
2019-07-07 14:43:01 +0300

    Fix slow execution. Timing

--- web-app/learn_flask.py
+++ web-app/learn_flask.py
@@ -5,11 +5,15 @@ sys.path.insert(1, '..')
 sys.path.insert(2, '../NN/RetinaNet')
 import os
 import infer_retinanet
+import time
 
 
 IMG_ROOT = 'static/upload'
 
+print("infer_retinanet.BrailleInference()")
+t = time.clock()
 recognizer = infer_retinanet.BrailleInference()
+print(time.clock()-t)
 
 app = Flask(__name__)
 
@@ -37,29 +41,37 @@ def upload():
 
 @app.route("/upload_results", methods=['GET', 'POST'])
 def save():
-    
+    t0 = time.clock()
+    print("save")
+    t = time.clock()
     # Save the image in the path
     if request.method == 'POST' and 'fileField' in request.files:
         filename = photos.save(request.files['fileField'])
-    
+    print(time.clock() - t)
+    print("recognizer.run")
+    t = time.clock()
     img_path = IMG_ROOT + "/" + filename
     out_img, lines, out_text = recognizer.run(img_path)
     out_filename = img_path+'.labeled.jpg'
+    print(time.clock() - t)
+    print("save")
+    t = time.clock()
     out_img.save(out_filename)
 
     with open(img_path+'.labeled.txt', 'w') as f:
         for s in out_text:
             f.write(s)
             f.write('\n')
-
+    print(time.clock() - t)
+    print(("total", time.clock() - t0) )
     #full_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'shovon.jpg')
 
     return render_template('display.html', filename=out_filename, letter=out_text)
 
 if __name__ == "__main__":
     debug = False
-
+    app.jinja_env.cache = {}
     if debug:
         app.run(debug=True)
     else:
-        app.run(host='0.0.0.0')
+        app.run(host='0.0.0.0', threaded=True)
