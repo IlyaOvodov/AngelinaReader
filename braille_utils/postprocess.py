eeab72b939c66f6896b624ae099979aaea4f7040
Ilya Ovodov <b@ovdv.ru>
2019-07-17 16:16:31 +0300

    lines v3

--- braille_utils/postprocess.py
+++ braille_utils/postprocess.py
@@ -20,6 +20,7 @@ class Line:
     SPACE_TO_H = 0.95 # 6.6/(2.7*2+1.5) = 0.96, 6.0/(2.5*2+1.3)=0.95, 6.5/(2.5*2+1.3) = 1.03
     LINE_THR = 0.5
     AVG_PERIOD = 5
+    MIN_AVG_PERIOD = 4
     def __init__(self, box, label):
         self.chars = []
         new_char = LineChar(box, label)
@@ -34,40 +35,35 @@ class Line:
         y = (box[1] + box[3])/2
         if abs(self.y + self.slip*(x-self.x) -y) < self.h*self.LINE_THR:
             new_char = LineChar(box, label)
-            if len(self.chars) >= self.AVG_PERIOD:
+            if len(self.chars) >= self.MIN_AVG_PERIOD:
                 calc_chars = self.chars[-self.AVG_PERIOD:]
                 best_pair = [1e99, None, None, None, None, ] # err, i,j,k,a,b
-                for i in range(self.AVG_PERIOD-3):
-                    for j in range(i+3, self.AVG_PERIOD):
+                for i in range(len(calc_chars)-2):
+                    for j in range(i+2, len(calc_chars)):
                         a = (calc_chars[j].y - calc_chars[i].y) / (calc_chars[j].x - calc_chars[i].x)
                         b = (calc_chars[i].y * calc_chars[j].x - calc_chars[i].x * calc_chars[j].y) / (calc_chars[j].x - calc_chars[i].x)
-                        ks = [k for k in range(self.AVG_PERIOD) if k!=i and k!= j]
+                        ks = [k for k in range(len(calc_chars)) if k!=i and k!= j]
                         errors = [abs(calc_chars[k].x * a + b - calc_chars[k].y) for k in ks]
                         min_err = min(errors)
                         if min_err < best_pair[0]:
                             idx_if_min = min(range(len(errors)), key=errors.__getitem__)
                             best_pair = min_err, i, j, ks[idx_if_min], a, b
                 _, i, j, k, a, b = best_pair
-                steps_between = j - i + sum([ch.spaces_before for ch in calc_chars[i+1: j+1]])
-                step = (calc_chars[j].x-calc_chars[i].x)/steps_between
-                reference_x = (calc_chars[j].x + calc_chars[i].x + steps_between*step)/2
-                steps_to_reference = round((new_char.x - reference_x) / step)
-                expected_x = reference_x + step*steps_to_reference
+                expected_x = new_char.x
                 expected_y = expected_x * a + b
                 w, h = (calc_chars[i].w + calc_chars[j].w + calc_chars[k].w)/3, (calc_chars[i].h + calc_chars[j].h + calc_chars[k].h)/3
                 new_char.refined_box = [expected_x-w/2, expected_y-h/2, expected_x+w/2, expected_y+h/2]
-                new_char.spaces_before = max(0, round( (expected_x - 0.5*(self.chars[-1].refined_box[0] + self.chars[-1].refined_box[2]))/step )-1)
                 self.x, self.y = expected_x, expected_y
                 self.h = h
                 self.slip = a
             else:
-                assert len(self.chars) >= 1
-                step = new_char.h * self.SPACE_TO_H
-                new_char.spaces_before = max(0, round((new_char.x - self.chars[-1].x)/step)-1)
                 self.x = new_char.x
                 self.y = new_char.y
                 self.h = new_char.h
                 self.slip = 0
+            step = self.h * self.SPACE_TO_H
+            assert len(self.chars) >= 1
+            new_char.spaces_before = max(0, round(0.5 * ((new_char.refined_box[0] + new_char.refined_box[2]) - (self.chars[-1].refined_box[0] + self.chars[-1].refined_box[2])) / step) - 1)
             self.chars.append(new_char)
             return True
         return False
