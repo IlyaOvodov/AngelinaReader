3fd6196474e6b58e5aa9278cfc18e14a3df4186f
Ilya Ovodov <b@ovdv.ru>
2019-07-23 13:53:37 +0300

    Удаление глобальных переменных

--- web-app/angelina_reader_app.py
+++ web-app/angelina_reader_app.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 from flask import Flask, render_template, redirect, request, url_for, flash
-from flask_login import LoginManager, current_user, login_user, logout_user
+from flask_login import LoginManager, current_user, login_user, logout_user, login_required
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, FileField, TextAreaField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, FileField, TextAreaField, HiddenField
 from wtforms.validators import DataRequired
 
 import time
@@ -52,6 +52,7 @@ class User:
         self.e_mail = e_mail
         self.name = name
         if is_new:
+            assert e_mail not in all_users.keys()
             all_users[e_mail] = {'name':name}
             with open(users_file, 'w') as f:
                 json.dump(all_users, f, sort_keys=True, indent=4)
@@ -66,17 +67,9 @@ def load_user(user_id):
         user = User(user_id, user["name"], is_new=False)
     return user
 
-img_path = ""
-marked_image_path=""
-out_text=""
-has_public_confirm = False
-
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/index", methods=['GET', 'POST'])
 def index():
-    global img_path
-    global has_public_confirm
-
     class MainForm(FlaskForm):
         file = FileField("Загрузить файл")
         agree = BooleanField("Я согласен")
@@ -86,73 +79,69 @@ def index():
     if form.validate_on_submit():
         if not form.file.data:
             flash('Необходимо загрузить файл')
-            return redirect(url_for('index'))
+            return render_template('index.html', form=form)
         if form.agree.data and form.disgree.data or not form.agree.data and not form.disgree.data:
             flash('Выберите один из двух вариантов')
-            return redirect(url_for('index'))
+            return render_template('index.html', form=form)
         os.makedirs(IMG_ROOT, exist_ok=True)
         filename = photos.save(form.file.data)
         img_path = IMG_ROOT + "/" + filename
         has_public_confirm = form.agree.data
 
         if not form.agree.data:
-            return redirect(url_for('confirm'))
-        return redirect(url_for('results'))
+            return redirect(url_for('confirm', img_path=img_path))
+        return redirect(url_for('results', img_path=img_path, has_public_confirm=has_public_confirm))
 
-    return render_template('index.html', current_user = current_user, form=form)
+    return render_template('index.html', form=form)
 
 
 @app.route("/confirm", methods=['GET', 'POST'])
+@login_required
 def confirm():
-    global img_path
-    global has_public_confirm
-
     class Form(FlaskForm):
+        img_path = HiddenField()
         agree = BooleanField("Я согласен")
         disgree = BooleanField("Возражаю", default=True)
         submit = SubmitField('Распознать')
-
     form = Form()
     if form.validate_on_submit():
         if form.agree.data and form.disgree.data or not form.agree.data and not form.disgree.data:
             flash('Выберите один из двух вариантов')
-            return redirect(url_for('index'))
+            return render_template('confirm.html', form=form)
         has_public_confirm = form.agree.data
-        return redirect(url_for('results'))
-    return render_template('confirm.html', current_user=current_user, form=form, )
-
+        return redirect(url_for('results', img_path=request.form['img_path'], has_public_confirm=has_public_confirm))
+    form = Form(img_path = request.values['img_path'])
+    return render_template('confirm.html', form=form)
 
 @app.route("/results", methods=['GET', 'POST'])
+@login_required
 def results():
-    global marked_image_path
-    global out_text
-    extra_info = {'user': current_user.get_id(), 'has_public_confirm': has_public_confirm}
-    marked_image_path, out_text = recognizer.run_and_save(img_path, RESULTS_ROOT, lang='RU', extra_info=extra_info,
-                                                          draw_refined=recognizer.DRAW_REFINED)  # TODO  accept lang
-    return render_template('display.html', filename=marked_image_path, letter=out_text, current_user = current_user)
-
-
-@app.route("/correct", methods=['GET', 'POST'])
-def correct():
-    class Form(FlaskForm):
-        text = TextAreaField(default='\n'.join(out_text))
+    class ResultsForm(FlaskForm):
+        marked_image_path = HiddenField()
+        text = TextAreaField()
         submit = SubmitField('Записать!')
-    form = Form()
+    form = ResultsForm()
     if form.validate_on_submit():
-        filename_stem = os.path.splitext(os.path.basename(img_path))[0]
+        filename_stem = os.path.splitext(os.path.basename(form.marked_image_path.data))[0]
         os.makedirs(CORR_RESULTS_ROOT, exist_ok=True)
-        path = CORR_RESULTS_ROOT + "/" + filename_stem + '.marked' + '.txt'
-
+        path = CORR_RESULTS_ROOT + "/" + filename_stem + '.txt'
         with open(path, 'w') as f:
             f.write(form.text.data)
         flash('СПАСИБО!')
         return redirect(url_for('index'))
-    return render_template('correct.html', filename=marked_image_path, form=form, current_user = current_user)
+
+    extra_info = {'user': current_user.get_id(), 'has_public_confirm': request.values['has_public_confirm']}
+    marked_image_path, out_text = recognizer.run_and_save(request.values['img_path'], RESULTS_ROOT, lang='RU', extra_info=extra_info,
+                                                          draw_refined=recognizer.DRAW_REFINED)  # TODO  accept lang
+    out_text = '\n'.join(out_text)
+    form = ResultsForm(marked_image_path=marked_image_path,
+                       text = out_text)
+    return render_template('correct.html', filename=marked_image_path, form=form)
 
 
 @app.route("/help")
 def help():
-    return render_template('help.html', current_user = current_user)
+    return render_template('help.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
