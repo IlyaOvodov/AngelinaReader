Fix rotation, fix json encoding

braille_utils/postprocess.py
@@ -1,8 +1,11 @@
-from functools import cmp_to_key
+import cv2
 from collections import defaultdict
+from functools import cmp_to_key
+import numpy as np
+import PIL
 
-from . import letters
-from . import label_tools as lt
+from braille_utils import letters
+from braille_utils import label_tools as lt
 
 
 class LineChar:
@@ -381,6 +384,290 @@ def validate_postprocess(in_text, out_text):
     '''
     res_text = lines_to_text(text_to_lines(in_text))
     assert res_text == out_text, (in_text, res_text, out_text)
+
+
+#####################################
+# find transformation
+#####################################
+
+MIN_RECTS = 10
+MAX_ROTATION = 0.2
+MIN_ROTATION = 0.02
+
+def center_of_char(ch):
+    """
+    :return: x,y - center of ch.refined_box
+    """
+    return (ch.refined_box[0] + ch.refined_box[2])/2, (ch.refined_box[1] + ch.refined_box[3])/2
+
+def find_line(ch1, ch2):
+    """
+    defines line than crossing centers of 2 chars: ax + by = 1
+    :param ch1:
+    :param ch2:
+    :return: a,b
+    """
+    x1, y1 = center_of_char(ch1)
+    x2, y2 = center_of_char(ch2)
+    d = x1*y2 - x2*y1
+    return (y2 - y1)/d, (x1 - x2)/d
+
+
+def find_cross(ln1, ln2):
+    """
+    finds cross points of 2 lines. Lines are defined as (a,b): ax+by=1
+    :param ln1:
+    :param ln2:
+    :return: (x,y)
+    """
+    d = ln1[0]*ln2[1] - ln2[0]*ln1[1]
+    return (ln2[1]-ln1[1])/d, (ln1[0]-ln2[0])/d
+
+
+def calc_v_err(ch, line):
+    """
+    calculates error between ch and horizontal line. If ch is not refered to line, return None
+    :param ch:
+    :param line:
+    :return: err
+    """
+    w = ch.refined_box[2] - ch.refined_box[0]
+    thr = w*0.25
+    x, y = center_of_char(ch)
+    yv = (1 - line[0]*x) / line[1]
+    if abs(yv - y) > thr:
+        return 1.
+    else:
+        return abs(yv - y) / w
+
+
+def calc_h_err(ch, line):
+    """
+    calculates error between ch and vertical line. If ch is not refered to line, return None
+    :param ch:
+    :param line:
+    :return: err
+    """
+    w = ch.refined_box[2] - ch.refined_box[0]
+    thr = w*0.25
+    x, y = center_of_char(ch)
+    xv = (1 - line[1]*y) / line[0]
+    if xv < ch.refined_box[0] or xv > ch.refined_box[2]:
+        return None
+    if abs(xv - x) > thr:
+        return 1.
+    else:
+        return abs(xv - x) / w
+
+
+
+def find_best_h_line(chars, bounds):
+    """
+    finds best horisontal line for line of chars
+    :param chars:
+    :param bounds:
+    :return: best_err, best_line
+    """
+    best_err = None
+    best_line = None
+    if len(chars) >= MIN_RECTS:
+        w = bounds[2] - bounds[0]
+        for left_ch in chars:
+            if left_ch.refined_box[0] > bounds[0] + w / 3:
+                break
+            for right_ch in reversed(chars):
+                if right_ch.refined_box[2] < bounds[2] - w / 3:
+                    break
+                ln = find_line(left_ch, right_ch)
+                err = 0
+                n = 0
+                for ch in chars:
+                    err_i = calc_v_err(ch, ln)
+                    if err_i is not None:
+                        err += err_i
+                        n += 1
+                if n >= MIN_RECTS:
+                    err /= n
+                    if best_err is None or err < best_err:
+                        best_err = err
+                        best_line = ln
+    return best_err, best_line
+
+
+def find_best_v_lines(top_ln, bot_ln, lines, bounds):
+    """
+    finds best 2 vertical lines for lines of chars
+    :return:
+    """
+    best_err = [None] * 2
+    best_line = [None] * 2
+    if len(lines) >= MIN_RECTS:
+        w = bounds[2] - bounds[0]
+        location = 0  # left or right side
+        for top_ch in top_ln.chars:
+            if top_ch.refined_box[0] >  bounds[0] + w / 3:
+                if top_ch.refined_box[2] <  bounds[2] - w / 3:
+                    continue
+                else:
+                    location = 1
+            topx, topy = center_of_char(top_ch)
+            bot_gen = bot_ln.chars if location == 0 else reversed(bot_ln.chars)
+            for bottom_ch in bot_gen:
+                botx, boty = center_of_char(bottom_ch)
+                if location == 0:
+                    if botx < topx - MAX_ROTATION * (boty - topy):
+                        continue
+                    if botx > topx + MAX_ROTATION * (boty - topy):
+                        break
+                else:
+                    if botx > topx + MAX_ROTATION * (boty - topy):
+                        continue
+                    if botx < topx - MAX_ROTATION * (boty - topy):
+                        break
+                ln = find_line(top_ch, bottom_ch)
+                err = 0
+                n = 0
+                for i_ln in lines:
+                    for ch in i_ln.chars:
+                        err_i = calc_h_err(ch, ln)
+                        if err_i is not None:
+                            err += err_i
+                            n += 1
+                    if n >= MIN_RECTS:
+                        err /= n
+                        if best_err[location] is None or err < best_err[location]:
+                            best_err[location] = err
+                            best_line[location] = ln
+    return best_err[0], best_line[0], best_err[1], best_line[1]
+
+
+def find_transformation_full(lines):
+    """
+    Finds alignment transform. Very slow and distortionable
+    :param lines:
+    :return:
+    """
+    if len(lines) == 0:
+        return
+    bounds = lines[0].chars[0].refined_box
+    for ln in lines:
+        for ch in ln.chars:
+            bounds = [min(bounds[0], ch.refined_box[0]), min(bounds[1], ch.refined_box[1]),
+                      max(bounds[2], ch.refined_box[2]), max(bounds[3], ch.refined_box[3])]
+    best_lines = [None] * 4  # left, top, right, bottom lines
+    best_errors = [None] * 4  # errors for best_lines
+    bot_lines = len(lines) // 4 if len(lines) >= MIN_RECTS else 0
+    top_lines = len(lines) // 4 if bot_lines > 0 else len(lines)
+    for top_i in range(top_lines):
+        top_ln = lines[top_i]
+        err, ln = find_best_h_line(top_ln.chars, bounds)
+        if err is not None:
+            if best_errors[1] is None or err < best_errors[1]:
+                best_errors[1], best_lines[1] = err, ln
+        for bot_i in range(len(lines) - bot_lines, len(lines)):
+            bot_ln = lines[bot_i]
+            err, ln = find_best_h_line(bot_ln.chars, bounds)
+            if err is not None:
+                if best_errors[3] is None or err < best_errors[3]:
+                    best_errors[3], best_lines[3] = err, ln
+            err1, ln1, err2, ln2 = find_best_v_lines(top_ln, bot_ln, lines, bounds)
+            if err1 is not None:
+                if best_errors[0] is None or err1 < best_errors[0]:
+                    best_errors[0], best_lines[0] = err1, ln1
+            if err2 is not None:
+                if best_errors[2] is None or err2 < best_errors[2]:
+                    best_errors[2], best_lines[2] = err2, ln2
+    print(best_errors)
+    if best_errors[0] is not None and best_errors[1] is not None and best_errors[2] is not None and best_errors[3] is not None:
+        src_points = np.array([find_cross(best_lines[0], best_lines[1]), find_cross(best_lines[2], best_lines[1]),   # 0 1
+                               find_cross(best_lines[0], best_lines[3]), find_cross(best_lines[2], best_lines[3])])  # 2 3
+        src_bounds = [min(src_points[0,0], src_points[2,0]), min(src_points[0,1], src_points[1,1]),
+                      max(src_points[1,0], src_points[3,0]), max(src_points[2,1], src_points[3,1]),]  # left top r. bot.
+        target_points = np.array([(src_bounds[0], src_bounds[1]), (src_bounds[2], src_bounds[1]),
+                                  (src_bounds[0], src_bounds[3]), (src_bounds[2], src_bounds[3])])
+        hom, mask = cv2.findHomography(src_points, target_points)
+    elif best_errors[1] is not None:
+        angle = -best_lines[1][0]/best_lines[1][1]*59  # rad -> grad
+        hom = cv2.getRotationMatrix2D(((bounds[0]+bounds[2])/2, (bounds[1]+bounds[3])/2), angle, 1.)
+    else:
+        hom = None
+    return hom
+
+
+def find_transformation(lines):
+    """
+    Finds alignment transform
+    :param lines:
+    :return:
+    """
+    hom = None
+    if len(lines) > 0:
+        bounds = lines[0].chars[0].refined_box
+        for ln in lines:
+            for ch in ln.chars:
+                bounds = [min(bounds[0], ch.refined_box[0]), min(bounds[1], ch.refined_box[1]),
+                          max(bounds[2], ch.refined_box[2]), max(bounds[3], ch.refined_box[3])]
+        sum_slip = 0
+        sum_weights = 0
+        for ln in lines:
+            if len(ln.chars) > MIN_RECTS:
+                pt1 = center_of_char(ln.chars[len(ln.chars)//5])
+                pt2 = center_of_char(ln.chars[len(ln.chars)*4//5])
+                dx = pt2[0] - pt1[0]
+                slip = (pt2[1] - pt1[1])/dx
+                sum_slip += slip * dx
+                sum_weights += dx
+        if sum_weights > 0:
+            angle = sum_slip / sum_weights  # rad -> grad
+            if abs(angle) < MAX_ROTATION and abs(angle) > MIN_ROTATION:
+                scale = 1 - angle*0.5  # 0.5 is empiric
+                hom = cv2.getRotationMatrix2D(((bounds[0] + bounds[2]) / 2, (bounds[1] + bounds[3]) / 2), angle * 59, scale)
+    return hom
+
+
+def transform(img, lines, rects, hom):
+    """
+    transforms img and refined_box'es and original_box'es for chars at lines using homography matrix found by
+    find_transformation(lines)
+    :param img: PIL image
+    :param lines:
+    :param hom:
+    :return: img, lines transformed
+    """
+    if hom.shape[0] == 3:
+        img_transform = cv2.warpPerspective
+        pts_transform = cv2.perspectiveTransform
+    else:
+        img_transform = cv2.warpAffine
+        pts_transform = cv2.transform
+    img = img_transform(np.asarray(img), hom, img.size, flags=cv2.INTER_LINEAR )
+    img = PIL.Image.fromarray(img)
+
+    old_centers = np.array([[center_of_char(ch) for ln in lines for ch in ln.chars]])
+    new_centers = pts_transform(old_centers, hom)
+    shifts = (new_centers - old_centers)[0]
+    i = 0
+    for ln in lines:
+        for ch in ln.chars:
+            ch.refined_box[0] += shifts[i, 0]
+            ch.refined_box[1] += shifts[i, 1]
+            ch.refined_box[2] += shifts[i, 0]
+            ch.refined_box[3] += shifts[i, 1]
+            ch.original_box[0] += shifts[i, 0]
+            ch.original_box[1] += shifts[i, 1]
+            ch.original_box[2] += shifts[i, 0]
+            ch.original_box[3] += shifts[i, 1]
+            i += 1
+
+    if len(rects):
+        old_centers = np.array([[((r[2]-r[0])/2, (r[3]-r[1])/2) for r in rects]])
+        new_centers = pts_transform(old_centers, hom)
+        shifts = (new_centers - old_centers)[0].tolist()
+        rects = [
+            (x[0][0] + x[1][0], x[0][1] + x[1][1], x[0][2] + x[1][0], x[0][3] + x[1][1])
+            for x in zip(rects, shifts)
+        ]
+    return img, lines, rects
 
 
 if __name__ == '__main__':

model/infer_retinanet.py
@@ -91,6 +91,7 @@ class BraileInferenceImpl(torch.nn.Module):
 
     def forward(self, input_tensor, input_tensor_rotated, find_orientation, process_2_sides):
         # type: (Tensor, Tensor, int)->Tuple[Tensor,Tensor,Tensor,int, Tuple[Tensor, Tensor, Tensor]]
+        t = time.clock()
         orientation_attempts = [OrientationAttempts.NONE]
         if find_orientation:
             orientation_attempts += [OrientationAttempts.ROT180, OrientationAttempts.ROT90, OrientationAttempts.ROT270]
@@ -112,21 +113,28 @@ class BraileInferenceImpl(torch.nn.Module):
                 input_data[OrientationAttempts.INV_ROT180] = torch.flip(-input_data[OrientationAttempts.ROT180], [3])
                 input_data[OrientationAttempts.INV_ROT90] = torch.flip(-input_data[OrientationAttempts.ROT90], [3])
                 input_data[OrientationAttempts.INV_ROT270] = torch.flip(-input_data[OrientationAttempts.ROT270], [3])
-        if self.verbose >= 2:
-            print("run.model")
         loc_preds: List[Tensor] = [torch.tensor(0)]*8
         cls_preds: List[Tensor] = [torch.tensor(0)]*8
+        if self.verbose >= 2:
+            print("        forward.prepare", time.clock() - t)
+            t = time.clock()
         for i, input_data_i in enumerate(input_data):
             if i in orientation_attempts:
                 loc_pred, cls_pred = self.model(input_data_i)
                 loc_preds[i] = loc_pred
                 cls_preds[i] = cls_pred
+        if self.verbose >= 2:
+            print("        forward.model", time.clock() - t)
+            t = time.clock()
         if find_orientation:
             best_idx, err_score = self.calc_letter_statistics(cls_preds, self.cls_thresh, orientation_attempts)
         else:
             best_idx, err_score = OrientationAttempts.NONE, (torch.tensor([0.]),torch.tensor([0.]),torch.tensor([0.]))
         if best_idx in [OrientationAttempts.INV, OrientationAttempts.INV_ROT180, OrientationAttempts.INV_ROT90, OrientationAttempts.INV_ROT270]:
             best_idx -= 2
+        if self.verbose >= 2:
+            print("        forward.calc_letter_statistics", time.clock() - t)
+            t = time.clock()
         h,w = input_data[best_idx].shape[2:]
         boxes, labels, scores = self.encoder.decode(loc_preds[best_idx][0].cpu().data,
                                                     cls_preds[best_idx][0].cpu().data, (w,h),
@@ -141,6 +149,9 @@ class BraileInferenceImpl(torch.nn.Module):
                                                            num_classes=self.num_classes)
         else:
             boxes2, labels2, scores2 = None, None, None
+        if self.verbose >= 2:
+            print("        forward.decode", time.clock() - t)
+            t = time.clock()
         return boxes, labels, scores, best_idx, err_score, boxes2, labels2, scores2
 
 
@@ -183,36 +194,66 @@ class BrailleInference:
         self.impl.to(device)
 
     def load_pdf(self, img_fn):
-        pdf = fitz.open(img_fn)
-        pdf = pdf.loadPage(0)
-        pdf = pdf.getPixmap()
-        cspace = pdf.colorspace
-        if cspace is None:
-            mode = "L"
-        elif cspace.n == 1:
-            mode = "L" if pdf.alpha == 0 else "LA"
-        elif cspace.n == 3:
-            mode = "RGB" if pdf.alpha == 0 else "RGBA"
-        else:
-            mode = "CMYK"
-        img = PIL.Image.frombytes(mode, (pdf.width, pdf.height), pdf.samples)
-        return img
+        try:
+            pdf_file = fitz.open(img_fn)
+            pg = pdf_file.loadPage(0)
+            pdf = pg.getPixmap()
+            cspace = pdf.colorspace
+            if cspace is None:
+                mode = "L"
+            elif cspace.n == 1:
+                mode = "L" if pdf.alpha == 0 else "LA"
+            elif cspace.n == 3:
+                mode = "RGB" if pdf.alpha == 0 else "RGBA"
+            else:
+                mode = "CMYK"
+            img = PIL.Image.frombytes(mode, (pdf.width, pdf.height), pdf.samples)
+            return img
+        except Exception as exc:
+            return None
 
-    def run(self, img_fn, lang, draw_refined, find_orientation, process_2_sides, gt_rects=[]):
+
+    def run(self, img_fn, lang, draw_refined, find_orientation, process_2_sides, repeat_on_aligned=True, gt_rects=[]):
         if gt_rects:
             assert find_orientation == False, "gt_rects можно передавать только если ориентация задана"
-        if self.verbose >= 2:
-            print("run.preprocess")
         t = time.clock()
         if Path(img_fn).suffix=='.pdf':
             img = self.load_pdf(img_fn)
         else:
             img = PIL.Image.open(img_fn)
+        if img is None:
+            return None
         if self.verbose >= 2:
-            print("run.preprocess", time.clock() - t)
-            print("run.make_batch")
-        t = time.clock()
+            print("run.reading image", time.clock() - t)
+            img.save(Path(results_dir) / 'original.jpg')
+            img.save(Path(results_dir) / 'original_100.jpg', quality=100)
+            t = time.clock()
+        if repeat_on_aligned and not process_2_sides:
+            results_dict0 = self.run_impl(img, lang, draw_refined, find_orientation,
+                                          process_2_sides=False, align=True, draw=False, gt_rects=gt_rects)
+            if self.verbose >= 2:
+                print("run.run_impl_1", time.clock() - t)
+                results_dict0['image'].save(Path(results_dir) / 're1.jpg')
+                results_dict0['image'].save(Path(results_dir) / 're1_100.jpg', quality=100)
+                t = time.clock()
+            results_dict = self.run_impl(results_dict0['image'], lang, draw_refined, find_orientation=False,
+                                         process_2_sides=process_2_sides, align=False, draw=True,
+                                         gt_rects=results_dict0['gt_rects'])
+            results_dict['best_idx'] = results_dict0['best_idx']
+            results_dict['err_scores'] = results_dict0['err_scores']
+            results_dict['homography'] = results_dict0['homography']
+        else:
+            results_dict = self.run_impl(img, lang, draw_refined, find_orientation,
+                                         process_2_sides=process_2_sides, align=True, draw=True, gt_rects=gt_rects)
+        if self.verbose >= 2:
+            results_dict['image'].save(Path(results_dir) / 're2.jpg')
+            results_dict['image'].save(Path(results_dir) / 're2_100.jpg', quality=100)
+            print("run.run_impl", time.clock() - t)
+        return results_dict
 
+
+    def run_impl(self, img, lang, draw_refined, find_orientation, process_2_sides, align, draw, gt_rects=[]):
+        t = time.clock()
         np_img = np.asarray(img)
         aug_img, aug_gt_rects = self.preprocessor.preprocess_and_augment(np_img, gt_rects)
         aug_img = data.unify_shape(aug_img)
@@ -226,46 +267,66 @@ class BrailleInference:
             aug_img_rot = data.unify_shape(aug_img_rot)
             input_tensor_rotated = self.preprocessor.to_normalized_tensor(aug_img_rot).to(device)
 
+        if self.verbose >= 2:
+            print("    run_impl.make_batch", time.clock() - t)
+            t = time.clock()
+
         with torch.no_grad():
             boxes, labels, scores, best_idx, err_score, boxes2, labels2, scores2 = self.impl(
                 input_tensor, input_tensor_rotated, find_orientation=find_orientation, process_2_sides=process_2_sides)
+        if self.verbose >= 2:
+            print("    run_impl.impl", time.clock() - t)
+            t = time.clock()
 
-        t = time.clock()
         boxes = boxes.tolist()
         labels = labels.tolist()
         scores = scores.tolist()
         lines = postprocess.boxes_to_lines(boxes, labels, lang = lang)
+
         if process_2_sides:
             boxes2 = boxes2.tolist()
             labels2 = labels2.tolist()
             scores2 = scores2.tolist()
             lines2 = postprocess.boxes_to_lines(boxes2, labels2, lang=lang)
 
-        if self.verbose >= 2:
-            print("run.postprocess", time.clock() - t)
-            print("run.draw")
-        t = time.clock()
-
         aug_img = PIL.Image.fromarray(aug_img if best_idx < OrientationAttempts.ROT90 else aug_img_rot)
         if best_idx in (OrientationAttempts.ROT180, OrientationAttempts.ROT270):
             aug_img = aug_img.transpose(PIL.Image.ROTATE_180)
 
-        raw_image = aug_img
+        if self.verbose >= 2:
+            print("    run_impl.postprocess", time.clock() - t)
+            aug_img.save(Path(results_dir) / 'aug_{}.jpg'.format(align))
+            aug_img.save(Path(results_dir) / 'aug_{}_100.jpg'.format(align), quality = 100)
+            t = time.clock()
+
+        if align and not process_2_sides:
+            hom = postprocess.find_transformation(lines)
+            if hom is not None:
+                aug_img, lines, aug_gt_rects = postprocess.transform(aug_img, lines, aug_gt_rects, hom)
+            if self.verbose >= 2:
+                print("    run_impl.align", time.clock() - t)
+                aug_img.save(Path(results_dir) / 'aligned_{}.jpg'.format(align))
+                aug_img.save(Path(results_dir) / 'aligned_{}_100.jpg'.format(align), quality = 100)
+                t = time.clock()
+        else:
+            hom = None
 
         results_dict = {
-            'image': raw_image,
+            'image': aug_img,
             'best_idx': best_idx,
             'err_scores': list([ten.cpu().data.tolist() for ten in err_score]),
             'gt_rects': aug_gt_rects,
+            'homography': hom.tolist() if hom is not None else hom,
         }
 
-        results_dict.update(self.draw_results(aug_img, boxes, lines, labels, scores, False, draw_refined))
-        if process_2_sides:
-            aug_img = aug_img.transpose(PIL.Image.FLIP_LEFT_RIGHT)
-            results_dict.update(self.draw_results(aug_img, boxes2, lines2, labels2, scores2, True, draw_refined))
+        if draw:
+            results_dict.update(self.draw_results(aug_img, boxes, lines, labels, scores, False, draw_refined))
+            if process_2_sides:
+                aug_img = aug_img.transpose(PIL.Image.FLIP_LEFT_RIGHT)
+                results_dict.update(self.draw_results(aug_img, boxes2, lines2, labels2, scores2, True, draw_refined))
+            if self.verbose >= 2:
+                print("    run_impl.draw", time.clock() - t)
 
-        if self.verbose >= 2:
-            print("run.draw", time.clock() - t)
         return results_dict
 
     def draw_results(self, aug_img, boxes, lines, labels, scores, reverse_page, draw_refined):
@@ -357,17 +418,16 @@ class BrailleInference:
 
 
     def run_and_save(self, img_path, results_dir, lang, extra_info, draw_refined,
-                     remove_labeled_from_filename, find_orientation, process_2_sides):
-        if self.verbose >= 2:
-            print("recognizer.run")
+                     remove_labeled_from_filename, find_orientation, process_2_sides, repeat_on_aligned):
         t = time.clock()
         result_dict = self.run(img_path, lang=lang, draw_refined=draw_refined,
                                find_orientation=find_orientation,
-                               process_2_sides=process_2_sides)
+                               process_2_sides=process_2_sides, repeat_on_aligned=repeat_on_aligned)
+        if result_dict is None:
+            return None
         if self.verbose >= 2:
-            print("recognizer.run", time.clock() - t)
-            print("save results")
-        t = time.clock()
+            print("run_and_save.run", time.clock() - t)
+            t = time.clock()
 
         os.makedirs(results_dir, exist_ok=True)
         filename_stem = os.path.splitext(os.path.basename(img_path))[0]
@@ -380,6 +440,7 @@ class BrailleInference:
                 ver = '20200816',
                 best_idx = result_dict['best_idx'],
                 err_scores = result_dict['err_scores'],
+                homography = result_dict['homography'],
                 model_weights = self.impl.model_weights_fn,
             )
             if extra_info:
@@ -391,11 +452,12 @@ class BrailleInference:
             results += [self.save_results(result_dict, True, results_dir, filename_stem)]
 
         if self.verbose >= 2:
-            print("save results", time.clock() - t)
+            print("run_and_save.save results", time.clock() - t)
         return results
 
     def process_dir_and_save(self, img_filename_mask, results_dir, lang, extra_info, draw_refined,
-                             remove_labeled_from_filename, find_orientation, process_2_sides):
+                             remove_labeled_from_filename, find_orientation, process_2_sides,
+                             repeat_on_aligned):
         if os.path.isfile(img_filename_mask) and os.path.splitext(img_filename_mask)[1] == '.txt':
             list_file = os.path.join(local_config.data_path, img_filename_mask)
             data_dir = os.path.dirname(list_file)
@@ -403,6 +465,9 @@ class BrailleInference:
                 files = f.readlines()
             img_files = [os.path.join(data_dir, fn[:-1] if fn[-1] == '\n' else fn) for fn in files]
             img_folders = [os.path.split(fn)[0] for fn in files]
+        elif os.path.isfile(img_filename_mask):
+            img_files = [img_filename_mask]
+            img_folders = [""]
         else:
             root_dir, mask = img_filename_mask.split('*', 1)
             mask = '*' + mask
@@ -416,12 +481,16 @@ class BrailleInference:
                 draw_refined=draw_refined,
 			    remove_labeled_from_filename=remove_labeled_from_filename,
                 find_orientation=find_orientation,
-                process_2_sides=process_2_sides)
+                process_2_sides=process_2_sides,
+                repeat_on_aligned=repeat_on_aligned)
+            if ith_result is None:
+                print('Error processing file: '+ str(img_file))
+                continue
             result_list += ith_result
         return result_list
 
     def run_and_save_archive(self, arch_path, results_dir, lang, extra_info, draw_refined,
-                    remove_labeled_from_filename, find_orientation, process_2_sides):
+                    remove_labeled_from_filename, find_orientation, process_2_sides, repeat_on_aligned):
         if os.path.isfile(img_filename_mask) and os.path.splitext(img_filename_mask)[1] == '.txt':
             list_file = os.path.join(local_config.data_path, img_filename_mask)
             data_dir = os.path.dirname(list_file)
@@ -434,29 +503,33 @@ class BrailleInference:
             mask = '*' + mask
             img_files = list(Path(root_dir).glob(mask))
             img_folders = [os.path.split(fn)[0].replace(str(Path(root_dir)), '')[1:] for fn in img_files]
-        return process_dir_and_save(self, img_filename_mask, results_dir, lang, extra_info=extra_info,
+        return self.process_dir_and_save(self, img_filename_mask, results_dir, lang, extra_info=extra_info,
                                     draw_refined=draw_refined,
                                     remove_labeled_from_filename=remove_labeled_from_filename,
                                     find_orientation=find_orientation,
-                                    process_2_sides=process_2_sides)
+                                    process_2_sides=process_2_sides,
+                                    repeat_on_aligned=repeat_on_aligned)
 
 
 if __name__ == '__main__':
 
-    img_filename_mask = r'D:\Programming.Data\Braille\Книги Анжелы\raw\Математика_ч2\*.jpg'
-    results_dir =       r'D:\Programming.Data\Braille\Книги Анжелы\Математика_ч2'
+    img_filename_mask = r'D:\Programming.Data\Braille\web_uploaded\data\raw\*.*'
+    #img_filename_mask = r'D:\Programming.Data\Braille\ASI\Braile Photos and Scans\Turlom_Copybook_3-18\Turlom_Copybook10\Photo_Turlom_C10\Photo_Turlom_C10_8.jpg'
+    #img_filename_mask = r'D:\Programming.Data\Braille\ASI\Student_Book\56-61\IMG_20191109_195953.jpg'
+    results_dir =       r'D:\Programming.Data\Braille\web_uploaded\re-processed200821'
+    #results_dir =       r'D:\Programming.Data\Braille\Temp\New'
 
-    img_filename_mask = r'D:\Programming.Data\Braille\Книги Анжелы\raw\3 класс\Математика_1/**/*.jpg'
-    results_dir =       r'D:\Programming.Data\Braille\Книги Анжелы\3 класс\Математика_1'
-
-    remove_labeled_from_filename = True
+    remove_labeled_from_filename = False
     find_orientation = True
-    process_2_sides = True
+    process_2_sides = False
+    repeat_on_aligned = False
+    verbose = 0
 
-    recognizer = BrailleInference()
+    recognizer = BrailleInference(verbose=verbose)
     recognizer.process_dir_and_save(img_filename_mask, results_dir, lang='RU', extra_info=None, draw_refined = recognizer.DRAW_NONE,
                                     remove_labeled_from_filename=remove_labeled_from_filename,
                                     find_orientation=find_orientation,
-                                    process_2_sides=process_2_sides)
+                                    process_2_sides=process_2_sides,
+                                    repeat_on_aligned=repeat_on_aligned)
 
     #recognizer.process_dir_and_save(r'D:\Programming.Data\Braille\My\raw\ang_redmi\*.jpg', r'D:\Programming.Data\Braille\tmp\flip_inv\ang_redmi', lang = 'RU')