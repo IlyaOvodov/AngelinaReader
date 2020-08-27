446e274a4ec39c91b3fc58710a2ac3eac2d7db30
Ilya Ovodov <b@ovdv.ru>
2020-08-28 01:31:32 +0300

    Added params.data.max_std parameter

--- data_utils/data.py
+++ data_utils/data.py
@@ -120,7 +120,7 @@ class ImagePreprocessor:
         assert img.ndim == 3
         ten_img = torch.from_numpy(img.transpose((2, 0, 1))).cuda().float()
         means = ten_img.view(3, -1).mean(dim=1)
-        std = torch.max(ten_img.view(3, -1).std(dim=1), torch.tensor(0.1*255).to(ten_img))
+        std = torch.max(ten_img.view(3, -1).std(dim=1), torch.tensor(self.params.data.max_std*255).to(ten_img))
                         #(ten_img.view(3, -1).max(dim=1)[0] - ten_img.view(3, -1).min(dim=1)[0])/6)
         ten_img = (ten_img - means.view(-1, 1, 1)) / (3*std.view(-1, 1, 1))
         # decolorize
--- model/params.py
+++ model/params.py
@@ -11,13 +11,14 @@ settings = AttrDict(
 
 params = AttrDict(
     data_root = local_config.data_path,
-    model_name = 'NN_results/retina_chars',
+    model_name = 'NN_results/retina_chars3_{model_params.encoder_params.aspect_ratios[0]}_{model_params.loss_params.class_loss_scale}_{augmentation.rotate_limit}',
     data = AttrDict(
         get_points = False,
         class_as_6pt=False,    # классификация присутствия каждой точки в рамке отдельно
         batch_size = 12,
         net_hw = (416, 416),
         rect_margin = 0.3, #  every of 4 margions to char width
+        max_std = 0.01,
         train_list_file_names = [
             r'DSBI/data/my_train.txt',
             r'My/labeled/labeled2/train_books.txt',
@@ -31,10 +32,10 @@ params = AttrDict(
             r'My/labeled/ASI/turlom_c2.txt',
         ],
         val_list_file_names = {
-            'val_dsbi' :  [
-                           r'DSBI/data/my_val1.txt',
-                           r'DSBI/data/my_val1.txt',
-                          ],
+            # 'val_dsbi' :  [
+            #                r'DSBI/data/my_val1.txt',
+            #                r'DSBI/data/my_val1.txt',
+            #               ],
             'val_2' : [
                             r'My/labeled/labeled2/val_books.txt',
                             r'My/labeled/labeled2/val_withtext.txt',
@@ -42,8 +43,12 @@ params = AttrDict(
                            ],
             'val_3_asi': [
                 r'My/labeled/ASI/turlom_c15_photo_1p.txt',
+                r'My/labeled/ASI/turlom_c15_photo_1p.txt',
+                r'My/labeled/ASI/turlom_c15_photo_1p.txt',
             ],
             'val_3_asi_scan': [
+                r'My/labeled/ASI/turlom_c15_scan_1p.txt',
+                r'My/labeled/ASI/turlom_c15_scan_1p.txt',
                 r'My/labeled/ASI/turlom_c15_scan_1p.txt',
             ],
         }
@@ -57,18 +62,18 @@ params = AttrDict(
     model_params = AttrDict(
         encoder_params = AttrDict(
             #anchor_areas = [5*5., 6*6., 10*10.,],
-            anchor_areas = [22*22*0.62, 33*33*0.62, 45*45*0.62,],
-            aspect_ratios=[0.62,],
+            anchor_areas=[22*22*0.62, 33*33*0.62, 45*45*0.62,], # [22*22*0.62, 33*33*0.62, 45*45*0.62,], #[8*16., 12*24., 16*32.,],
+            aspect_ratios=[0.62,],  # [0.62,], #[1 / 2.,],
             #aspect_ratios=[1.,],
             #scale_ratios=[1., pow(2, 1 / 3.), pow(2, 2 / 3.)]
             iuo_fit_thr = 0, # if iou > iuo_fit_thr => rect fits anchor
             iuo_nofit_thr = 0,
         ),
         loss_params=AttrDict(
-            #class_loss_scale = 100,
+            class_loss_scale = 100,
         ),
     ),
-    load_model_from = 'NN_saved/retina_chars_eced60/models/clr.008',
+    load_model_from = 'NN_results/retina_chars_d58e5f/models/clr.001.t7',  # retina_chars_d58e5f # retina_chars_7e1d4e
     optim = 'torch.optim.SGD',
     optim_params = AttrDict(
         lr=0.0001,
@@ -90,12 +95,10 @@ params = AttrDict(
     ),
     clr=AttrDict(
         warmup_epochs=10,
-        min_lr=1e-3,
-        max_lr=0.01,
+        min_lr=1e-4,
+        max_lr=0.09,
         period_epochs=500,
         scale_max_lr=0.95,
         scale_min_lr=0.95,
     ),
-
-    #decimate_lr_every = 1200,
 )
