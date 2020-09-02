9885c8d690be09ac26235a5d64bd659a5d5f5bcd
Ilya Ovodov <b@ovdv.ru>
2020-09-02 22:30:36 +0300

    local_config

--- local_config.py
+++ local_config.py
@@ -1,2 +1,8 @@
-data_path = r'/home/orwell/Data/Braille/'
-global_3rd_party = r'/home/orwell/Programming/3rd_party/'
+#!/usr/bin/env python
+# coding: utf-8
+"""
+Local settings
+"""
+from pathlib import Path
+data_path = str(Path(__file__).parent)  # root local data directory
+global_3rd_party = data_path  # root local 3rd_party directory
