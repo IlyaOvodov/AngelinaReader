86c3040a21183929e0ff5220999ebacabfe3e1ae
Ilya Ovodov <b@ovdv.ru>
2020-11-27 19:52:30 +0300

    FIX #5 More time for session timeout

--- web_app/config.py
+++ web_app/config.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'angilina'
@@ -8,3 +9,4 @@ class Config(object):
     SMTP_PORT = os.environ.get('SMTP_PORT') or "587"
     SMTP_PWD = os.environ.get('SMTP_PWD') or ""
     SMTP_FROM = os.environ.get('SMTP_FROM') or "results@angelina-reader.ru"
+    PERMANENT_SESSION_LIFETIME = datetime.timedelta(minutes=60*24*365*2)
