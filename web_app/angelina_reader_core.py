99a77d4a01ecfd2085cf904868cace5eee3a8112
Ilya Ovodov <b@ovdv.ru>
2021-02-18 20:12:46 +0300

    updated core for new interface

--- web_app/angelina_reader_core.py
+++ web_app/angelina_reader_core.py
@@ -3,8 +3,11 @@
 """
 Описание интерфейсов между UI и алгоритмическим модулями
 """
+from datetime import datetime
 import json
 import os
+import time
+import timeit
 import uuid
 
 class User:
@@ -36,12 +39,11 @@ class User:
     def get_id(self):
         return self.id        
         
-    def check_password(self, password):
+    def check_password(self, password_hash):
         """
         Проверяет пароль. Вызывать про логине по email.
         """
-        raise NotImplementedError
-        return True
+        return self.password_hash is None or self.password_hash == password_hash
 
     def set_name(self, name):
         """
@@ -61,7 +63,7 @@ class User:
         """
         Обновляет пароль. Вызывать про логине по email.
         """
-        raise NotImplementedError
+        # TODO raise NotImplementedError
         pass
         
     def get_param_default(self, param_name):
@@ -107,7 +109,7 @@ class UserManager:
         self._update_users_dict(id, new_user)
         return User(id, new_user)
 
-    def find_user(self, id=None, network_name=None, network_id=None, email=None):
+    def find_user(self, network_name=None, network_id=None, email=None, id=None):
         """
         Возвращает объект User по регистрационным данным: id или паре network_name+network_id или регистрации по email (для этого указать network_name = None или network_name = "")
         Если юзер не найден, возвращает None
@@ -124,7 +126,7 @@ class UserManager:
                 assert not network_name and not network_id
             found_user_dicts = dict()
             for id, user_dict in all_users.items():
-                if (network_name and user_dict["network_name"] == network_name and network_id and user_dict["network_id"] == network_id
+                if (network_name and user_dict.get("network_name") == network_name and network_id and user_dict.get("network_id") == network_id
                     or email and user_dict["email"] == email):
                     found_user_dicts[id] = user_dict
             assert len(found_user_dicts) <= 1, found_user_dicts
@@ -162,8 +164,53 @@ class AngelinaSolver:
     """
     Обеспечивает интерфейс с вычислительной системой: пользователи, задачи и результаты обработки
     """
-    def __init__(self, user_file_name):
+    def __init__(self, user_file_name="static/data/all_users.json"):
         self.user_manager = UserManager(user_file_name)
+
+    help_articles = ["test_about", "test_photo"]
+    help_contents = {
+        "rus": {
+            "test_about": {"title": "О программе",
+                           "announce": "Это очень крутая программа! <b>Не пожалеете</b>! Просто нажмите кнопку",
+                           "text": "Ну что вам еще надо. <b>Вы не верите</b>?"},
+            "test_photo": {"title": "Как сделать фото",
+                           "announce": "Чтобы сделать фото нужен фотоаппарат",
+                           "text": "Просто нажмите кнопку!"}
+        },
+        "eng": {
+            "test_about": {"title": "About",
+                           "announce": "It a stunning program! <b>Dont miss it</b>! Just press the button",
+                           "text": "Why don't you believe! What do you need <b>more</b>!"},
+            "test_photo": {"title": "How to make photo",
+                           "announce": "In order to make photo you need a camera",
+                           "text": "You really need. Believe me!"}
+        }
+    }
+
+    def help_list(self, target_language, search_qry):
+        """
+        Возвращаем список материалов для страницы help. Поскольку создавать html файл для каждой информационной статьи
+        не самая лучшая идея, это лучше делать через вывод материалов из БД
+        """
+        total_list = [{ **{tag: self.help_contents[target_language][slug][tag] for tag in ["title", "announce"]}, **{"slug": slug} }
+                for slug in self.help_articles]
+        if search_qry:
+            return total_list[:1]
+        return  total_list
+
+
+        # [
+        #     {"title": "О программе", "announce": "Это очень крутая программа! Не пожалеете! Просто нажмите кнопку", "slug": "test_about"},
+        #     {"title": "Как сделать фото", "announce": "Чтобы сделать фото нужен фотоаппарат", "slug": "test_photo"},
+        # ]
+
+    def help_item(self, target_language, slug):
+        """
+        Возвращаем материал для странии help.
+        """
+        return self.help_contents[target_language][slug]
+        # {"title":"name","text":"def_text"}
+
 
     #Работа с записями пользователей: создание (регистрация), обработка логина:
     def register_user(self, name, email, password_hash, network_name, network_id):
@@ -178,12 +225,12 @@ class AngelinaSolver:
         """
         return self.user_manager.register_user(name=name, email=email, password_hash=password_hash, network_name=network_name, network_id=network_id)
 
-    def find_user(self, id=None, network_name=None, network_id=None, email=None):
+    def find_user(self, network_name=None, network_id=None, email=None, id=None):
         """
         Возвращает объект User по регистрационным данным: паре network_name+network_id или регистрации по email (для этого указать network_name = None или network_name = "")
         Если юзер не найден, возвращает None
         """
-        return self.user_manager.find_user(id, network_name, network_id, email)
+        return self.user_manager.find_user(network_name, network_id, email, id)
 
     def find_users_by_email(self, email):
         """
@@ -193,11 +240,17 @@ class AngelinaSolver:
         """
         return self.user_manager.find_users_by_email(email)
     
-    
+    #GVNC
+    TMP_RESULT_SELECTOR = 1  # index in TMP_RESILTS
+    TMP_RESILTS = ['IMG_20210104_093412', 'IMG_20210104_093217']
+    PREFIX = "/static/data/results/"
+    TMP_TASK_POST_TIMES = {}
+    TMP_RECOG_DELAY = 2  # sec.
+
     # собственно распознавание
-    def process(self, user, img_paths, lang, find_orientation, process_2_sides, has_public_confirm, timeout):
+    def process(self, user_id, img_paths, lang, find_orientation, process_2_sides, has_public_confirm, timeout=0):
         """
-        user: User class object or None для анонимного доступа
+        user: User ID or None для анонимного доступа
         img_paths: полный пусть к загруженному изображению, pdf или zip или список (list) полных путей к изображению
         lang: выбранный пользователем язык ('RU', 'EN')
         find_orientation: bool, поиск ориентации
@@ -206,45 +259,88 @@ class AngelinaSolver:
         timeout: время ожидания результата. Если None или < 0 - жадть пока не будет завершена. 0 поставить в очередь и не ждать.
         
         Ставит задачу в очередь на распознавание и ждет ее завершения в пределах timeout.
-        Возвращает пару task_id (id задачи), завершена ли
+        После успешной загрузки возвращаем id материалов в системе распознавания или False если в процессе обработки 
+        запроса возникла ошибка. Далее по данному id мы переходим на страницу просмотра результатов данного распознавнаия
         """
-        raise NotImplementedError
-        return "fsgfdsdfsdf", True
+        if timeout and timeout > 0:
+            time.sleep(timeout)
+
+        AngelinaSolver.TMP_RESULT_SELECTOR = 1 - AngelinaSolver.TMP_RESULT_SELECTOR
+        AngelinaSolver.TMP_TASK_POST_TIMES[AngelinaSolver.TMP_RESILTS[AngelinaSolver.TMP_RESULT_SELECTOR]] = timeit.default_timer()
+        return AngelinaSolver.TMP_RESILTS[AngelinaSolver.TMP_RESULT_SELECTOR]
         
     def is_completed(self, task_id):
         """
         Проверяет, завершена ли задача с заданным id
         """
-        raise NotImplementedError
-        return True
-        
+        """
+        В тестовом варианте отображется как не готовый в течение 2 с после загрузки
+        """
+        assert task_id in AngelinaSolver.TMP_RESILTS
+        if task_id in AngelinaSolver.TMP_TASK_POST_TIMES.keys():
+            return timeit.default_timer() - AngelinaSolver.TMP_TASK_POST_TIMES[task_id] > AngelinaSolver.TMP_RECOG_DELAY
+        else:
+            return False
+
     def get_results(self, task_id):
         """
         Возвращает результаты распознавания по задаче task_id.
         Не проверяет, что задача была поставлена этим пользователем. Это ответственность вызывающей стороны.
         
-        Возвращает пару results_list, params.
-        results_list - список (list) результатов по количеству страниц в задании. Каждый элемени списка - tuple из полных путей к файлам с изображением, распознанным текстом, распознанным брайлем
-        params - полный путь к файлу с сохраненным словарем параметров распознавания
+        Возвращает словарь с полями:
+            {"name": str,
+             "create_date": datetime,
+             "protocol": путь к protocol.txt
+			 "item_data": список (list) результатов по количеству страниц в задании. 
+			 Каждый элемени списка - tuple из полных путей к файлам с изображением, распознанным текстом, распознанным брайлем
+            }
         """
-        raise NotImplementedError
-        return [("/res/file.marked.jpg", "/res/file.marked.txt", "/res/file.marked_braille.txt",),], "/res/file.params.txt"
-        
-    def get_tasks_list(self, user):
         """
+        В тестововм варианте по очереди выдается то 1 документ, то 2.
+        """
+        prefix = AngelinaSolver.PREFIX
+        return {"name":task_id,
+                "create_date": datetime.strptime('2011-11-04 00:05:23', "%Y-%m-%d %H:%M:%S"), #"20200104 200001",
+                "item_data":
+        [
+        (prefix + task_id + ".marked.jpg", prefix[1:] + task_id + ".marked.txt", prefix[1:] + task_id + ".marked.brl",),
+        (prefix + task_id + ".marked.jpg", prefix[1:] + task_id + ".marked-2.txt", prefix[1:] + task_id + ".marked.brl",),
+        ][:(AngelinaSolver.TMP_RESULT_SELECTOR+1)],  # возвращает 1 или 2 результаты попеременно
+        "protocol": prefix + task_id + ".protocol.txt"}
+
+    def get_tasks_list(self, user_id, count):
+        """
+        count - кол-во запиисей
         Возвращает список task_id задач для данного юзера, отсортированный от старых к новым
         """
-        raise NotImplementedError
-        return ["fsgfdsdfsdf"]
-        
-    def get_task_breif(self, task_id):
         """
-        Возвращает краткую информацию для заданнонй задачи для отображения в истории:
-        время, имя, маленькую картинку, первые 3 строки текста, признак публичной доступности, признак, что задача завершена.
-        TOFO открытый вопрос: 1) в каком формате время, 2) как возвращать картиннку (имя файла или битмап).
+        В тестовом варианте возвращает 10 раз взятый список из 2 демо-результатов.
+        При этом сначала все они показываются как не законченные. По мере моделирования расчетов показывается   
+        более реалистично: пример выдается как не готовый 2 сек после запуска распознавания
+        Публичный -приватный - через одного
         """
-        raise NotImplementedError
-        return "20200104 200001", "pic1609257409433.jpg", "/res/file.thumbnail.jpg", "буря\nмглою\nнебо", True, True
+        if not user_id or user_id == "false":
+            return []
+
+        lst = [
+                  {
+                    "id":task,
+                    "date": datetime.strptime('2011-11-04 00:05:23', "%Y-%m-%d %H:%M:%S"),  # "20200104 200001",  #datetime.fromisoformat('2011-11-04T00:05:23')
+                    "name":task + ".jpg",
+                    "img_url":"/static/data/results/pic.jpg",  # PIL.Image.Open("web_app/static/data/results/pic.jpg")
+                    #"desc":"буря\nмглою\nнебо",
+                    "desc":"I            B             101\nкоторые созвучны друг с дру-\r\nгом. например, в первых четырёх ~?~",
+                    "public": i%2 ==0,
+                    "sost": self.is_completed(task)
+                   }
+            for i, task in enumerate(AngelinaSolver.TMP_RESILTS)
+        ]*10
+
+        if count:
+            lst = lst[:count]
+        return lst
+
+
 
     CONTENT_IMAGE = 1
     CONTENT_TEXT = 2
@@ -252,23 +348,24 @@ class AngelinaSolver:
     CONTENT_ALL = CONTENT_IMAGE | CONTENT_TEXT | CONTENT_BRAILLE
 
     # отправка почты
-    def send_results_to_mail(self, results_list, to_email, send_to_developers=False, title="", comment="", content_options=CONTENT_ALL):
+    def send_results_to_mail(self, mail, item_id, parameters=None):
         """
         Отправляет результаты на to_email и/или разработчикам. Что-то одно - обязательно.
         results_list - список результатов такой же, как возвращает get_results(...)
         to_email - адрес или список адресов
-        title - заголовок
-        comment - комментарий, ставится в начале письма
-        content_options - опции, что выводить: картинку, тектс, текст на брайле или их комбинация (добавить enum  битовой маской)
+        parameters - словарь с параметрами формирования и отправки письма, в т.ч.:
+            title - заголовок
+            comment - комментарий, ставится в начале письма
         """
-        raise NotImplementedError
-        pass
-    
-    
-    
-        
-        
-    
+        # raise NotImplementedError
+        return True
 
-        
-    
+    def get_user_emails(self, user_id):
+        """
+        Список адресов почты, куда пользователь отсылал письма
+        :param user_id: string
+        :return: list of e-mails
+        """
+        if not user_id or user_id == "false":
+            return []
+        return ["angelina-reader@ovdv.ru", "il@ovdv.ru", "iovodov@gmail.com"]
