4019086c82633887cdda6730af17cc3952a1fb86
IlyaOvodov <b@ovdv.ru>
2023-03-22 17:17:36 +0000

    revert local_config

--- local_config.py
+++ local_config.py
@@ -5,5 +5,3 @@ Local settings
 from pathlib import Path
 data_path = str(Path(__file__).parent)  # root local data directory
 global_3rd_party = str(Path(__file__).parent)  # root local 3rd_party directory
-
-data_path = '/home/ovod/file_server/pub_data'
