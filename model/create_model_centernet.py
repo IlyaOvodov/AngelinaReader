8a16fd013aa6afdbce437701b2f4f6de6fbd282c
IlyaOvodov <b@ovdv.ru>
2024-10-12 12:24:28 +0000

    Fix default params handling

--- model/create_model_centernet.py
+++ model/create_model_centernet.py
@@ -12,50 +12,59 @@ from models.losses import RegL1Loss, RegLoss, NormRegL1Loss, RegWeightedL1Loss
 from models.utils import _sigmoid
 from utils.image import draw_umich_gaussian, draw_msra_gaussian, draw_dense_reg
 
-NUM_CLASSES = 64  # 64 1
-        
-def create_model_centernet(params, device):
-    opt = params.model_params
+NUM_CLASSES = 64
 
-    arch = opt.get('arch', 'dla_34')
-    head_conv = opt.get('head_conv', 256 if 'dla' in arch else 64)
-    heads = opt.get('heads', AttrDict(
+def update_params_with_defailts(opt):
+    opt = AttrDict(opt.copy())
+
+    opt['arch'] = opt.get('arch', 'dla_34')
+    opt['head_conv'] = opt.get('head_conv', 256 if 'dla' in opt.arch else 64)
+    opt['heads'] = opt.get('heads', AttrDict(
         hm = NUM_CLASSES, wh = 2, reg = 2,
     ))
-    model = create_model(arch, heads, head_conv)
+    if opt.use_hm1:
+        opt['heads']['hm1'] = 1
+    
+    opt['max_objs'] = 1000
+    opt['down_ratio'] = 4
+    opt['mse_loss'] = False
+    opt['reg_loss'] = 'l1'
+    opt['dense_wh'] = False
+    opt['norm_wh'] = False
+    opt['cat_spec_wh'] = False
+    opt['num_stacks'] = 1
+    opt['eval_oracle_hm'] = False
+    opt['eval_oracle_wh'] = False
+    opt['eval_oracle_offset'] = False
+    opt['reg_offset'] = True
+    opt['hm_weight'] = 1
+    opt['wh_weight'] = 0.1
+    opt['off_weight'] = 1
+    opt['hm1_weight'] = 1
+    return opt
+
+
+def create_model_centernet(params, device):
+    opt = update_params_with_defailts(params.model_params)
+
+    model = create_model(opt.arch, opt.heads, opt.head_conv)
     model = model.to(device)
 
     dataset_class = CenterNetDataset
 
     collate_fn = None
     
-    opt = opt.loss_params
-
-    opt.mse_loss = False
-    opt.reg_loss = 'l1'
-    opt.dense_wh = False
-    opt.norm_wh = False
-    opt.cat_spec_wh = False
-    opt.num_stacks = 1
-    opt.eval_oracle_hm = False
-    opt.eval_oracle_wh = False
-    opt.eval_oracle_offset = False
-    opt.reg_offset = True
-    opt.hm_weight = 1
-    opt.wh_weight = 0.1
-    opt.off_weight = 1
-    
     loss = CenterNetLoss(opt)
     loss = loss.to(device)
 
     return model, dataset_class, collate_fn, loss
 
+
 class CenterNetDataset(BrailleDataset):
     def __init__(self, params, *args, **kwargs):
         super().__init__(params, *args, **kwargs)
-        self.params = params
-        self.opt = params.model_params.loss_params  # compatibility with CenterNet params 
-        self.max_objs = 1000
+        self.opt = update_params_with_defailts(params.model_params)  # compatibility with CenterNet params 
+        self.max_objs = self.opt.max_objs
         
     def __getitem__(self, index):
         img_ten, bboxes, *other = super().__getitem__(index)
@@ -64,7 +73,7 @@ class CenterNetDataset(BrailleDataset):
         num_objs = len(bboxes)
 
         input_h, input_w = img_ten.shape[-2], img_ten.shape[-1]
-        down_ratio = self.params.model_params.get('down_ratio', 4)
+        down_ratio = self.opt.down_ratio
         output_h = input_h // down_ratio
         output_w = input_w // down_ratio
         num_classes = NUM_CLASSES
@@ -145,10 +154,11 @@ class CenterNetDecoder:
         """
         return boxes, labels, scores
         """
+        opt = update_params_with_defailts(params.model_params)
         hm = pred[-1]['hm'].sigmoid_()  # BCHW
         wh = pred[-1]['wh']
-        reg = pred[-1]['reg'] if params.model_params.loss_params.reg_offset else None
-        dets = ctdet_decode(hm, wh, reg=reg, cat_spec_wh=params.model_params.loss_params.cat_spec_wh, K=1000)  # Nx6: (bboxes, scores, classes)
+        reg = pred[-1]['reg'] if opt.reg_offset else None
+        dets = ctdet_decode(hm, wh, reg=reg, cat_spec_wh=opt.cat_spec_wh, K=1000)  # BxNx6: (bboxes, scores, classes)
         dets = dets[0] # remove batch
         cls_thr = params.inference_params.cls_thresh
         dets = dets[dets[:,4] >= cls_thr]
@@ -165,12 +175,14 @@ class CenterNetDecoder:
 class CtdetLoss(torch.nn.Module):
     def __init__(self, opt):
         super(CtdetLoss, self).__init__()
-        self.crit = torch.nn.MSELoss() if opt.mse_loss else FocalLoss()
+        opt = update_params_with_defailts(opt)
+        self.opt = opt
+        self.crit = torch.nn.MSELoss() if opt.mse_loss else FocalLoss() # FocalLoss() <-- False
         self.crit_reg = (
-            RegL1Loss()
+            RegL1Loss()  # <--
             if opt.reg_loss == "l1"
             else RegLoss()
-            if opt.reg_loss == "sl1"
+            if opt.reg_loss == "sl1"  # ? better (use smooth L1 instead of L1)
             else None
         )
         self.crit_wh = (
@@ -180,21 +192,36 @@ class CtdetLoss(torch.nn.Module):
             if opt.norm_wh
             else RegWeightedL1Loss()
             if opt.cat_spec_wh
-            else self.crit_reg
+            else self.crit_reg  # RegL1Loss() <--
         )
-        self.opt = opt
+
 
     def forward(self, outputs, batch):
+        """
+        Args:
+            outputs (_type_):
+                output["hm"]: class map: BCHW
+                output["wh"]: size(wh) map: B2HW
+                output["reg"]: delta(x,y) centers by objects: B2HW
+            batch (_type_):
+                batch["hm"] - gt gaussed class map: BCHW
+                batch["wh"] - gt size(wh) by objects: BM2 (M = max_obj)
+                batch["reg"] - gt delta(x,y) centers by objects: BM2 (M = max_obj)
+                batch['reg_mask'] - object presence among max_obj: BM (1,1, ..., 1,0, ..., 0)
+                batch['ind'] - linear index of obj. centers ion map: BM
+        Returns:
+            loss, loss_stats
+        """
         opt = self.opt
         hm_loss, wh_loss, off_loss = 0, 0, 0
         for s in range(opt.num_stacks):
             output = outputs[s]
             if not opt.mse_loss:
-                output["hm"] = _sigmoid(output["hm"])
+                output["hm"] = _sigmoid(output["hm"])  # sigmoid + clamp
 
-            if opt.eval_oracle_hm:
+            if opt.eval_oracle_hm:  # False
                 output["hm"] = batch["hm"]
-            if opt.eval_oracle_wh:
+            if opt.eval_oracle_wh:  # False
                 output["wh"] = torch.from_numpy(
                     gen_oracle_map(
                         batch["wh"].detach().cpu().numpy(),
@@ -203,7 +230,7 @@ class CtdetLoss(torch.nn.Module):
                         output["wh"].shape[2],
                     )
                 ).to(opt.device)
-            if opt.eval_oracle_offset:
+            if opt.eval_oracle_offset:  # False
                 output["reg"] = torch.from_numpy(
                     gen_oracle_map(
                         batch["reg"].detach().cpu().numpy(),
@@ -274,11 +301,13 @@ def _gather_feat(feat, ind, mask=None):
         feat = feat.view(-1, dim)
     return feat
 
+
 def _transpose_and_gather_feat(feat, ind):
     feat = feat.permute(0, 2, 3, 1).contiguous()
     feat = feat.view(feat.size(0), -1, feat.size(3))
     feat = _gather_feat(feat, ind)
     return feat
+
 
 def _nms(heat, kernel=3):
     pad = (kernel - 1) // 2
@@ -287,6 +316,7 @@ def _nms(heat, kernel=3):
         heat, (kernel, kernel), stride=1, padding=pad)
     keep = (hmax == heat).float()
     return heat * keep
+
 
 def _topk(scores, K=40):
     batch, cat, height, width = scores.size()
@@ -305,6 +335,7 @@ def _topk(scores, K=40):
     topk_xs = _gather_feat(topk_xs.view(batch, -1, 1), topk_ind).view(batch, K)
 
     return topk_score, topk_inds, topk_clses, topk_ys, topk_xs
+
 
 def ctdet_decode(heat, wh, reg=None, cat_spec_wh=False, K=100):
     batch, cat, height, width = heat.size()
@@ -338,4 +369,3 @@ def ctdet_decode(heat, wh, reg=None, cat_spec_wh=False, K=100):
     detections = torch.cat([bboxes, scores, clses], dim=2)
       
     return detections
-
