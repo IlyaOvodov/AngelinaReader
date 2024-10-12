4d4f4d2edbaed03169bd3261b3da944228900eb5
IlyaOvodov <b@ovdv.ru>
2024-10-12 12:16:48 +0000

    Copy-paste code from CenterNet to local

--- model/create_model_centernet.py
+++ model/create_model_centernet.py
@@ -7,8 +7,9 @@ from data_utils.data import BrailleDataset
 
 # CenterNet project
 from models.model import create_model
-from models.decode import ctdet_decode
-from trains.ctdet import CtdetLoss
+from models.losses import FocalLoss
+from models.losses import RegL1Loss, RegLoss, NormRegL1Loss, RegWeightedL1Loss
+from models.utils import _sigmoid
 from utils.image import draw_umich_gaussian, draw_msra_gaussian, draw_dense_reg
 
 NUM_CLASSES = 64  # 64 1
@@ -115,6 +116,7 @@ class CenterNetDataset(BrailleDataset):
             ret.update({'reg': reg})
         return img_ten, ret
 
+
 class CenterNetLoss(nn.Module):
     def __init__(self, opt):
         super().__init__()
@@ -129,6 +131,7 @@ class CenterNetLoss(nn.Module):
         def call(*kargs, **kwargs):
             return self.loss_dict.get(key, torch.zeros(1)[0])
         return call
+
 
 class CenterNetDecoder:
     def __init__(self, params):
@@ -153,3 +156,186 @@ class CenterNetDecoder:
         boxes[:, [0,2]] *= size_wh[0]/hm.shape[-1]
         boxes[:, [1,3]] *= size_wh[1]/hm.shape[-2]
         return boxes, labels, scores
+
+
+###############
+# copied from CenterNet project
+###############
+
+class CtdetLoss(torch.nn.Module):
+    def __init__(self, opt):
+        super(CtdetLoss, self).__init__()
+        self.crit = torch.nn.MSELoss() if opt.mse_loss else FocalLoss()
+        self.crit_reg = (
+            RegL1Loss()
+            if opt.reg_loss == "l1"
+            else RegLoss()
+            if opt.reg_loss == "sl1"
+            else None
+        )
+        self.crit_wh = (
+            torch.nn.L1Loss(reduction="sum")
+            if opt.dense_wh
+            else NormRegL1Loss()
+            if opt.norm_wh
+            else RegWeightedL1Loss()
+            if opt.cat_spec_wh
+            else self.crit_reg
+        )
+        self.opt = opt
+
+    def forward(self, outputs, batch):
+        opt = self.opt
+        hm_loss, wh_loss, off_loss = 0, 0, 0
+        for s in range(opt.num_stacks):
+            output = outputs[s]
+            if not opt.mse_loss:
+                output["hm"] = _sigmoid(output["hm"])
+
+            if opt.eval_oracle_hm:
+                output["hm"] = batch["hm"]
+            if opt.eval_oracle_wh:
+                output["wh"] = torch.from_numpy(
+                    gen_oracle_map(
+                        batch["wh"].detach().cpu().numpy(),
+                        batch["ind"].detach().cpu().numpy(),
+                        output["wh"].shape[3],
+                        output["wh"].shape[2],
+                    )
+                ).to(opt.device)
+            if opt.eval_oracle_offset:
+                output["reg"] = torch.from_numpy(
+                    gen_oracle_map(
+                        batch["reg"].detach().cpu().numpy(),
+                        batch["ind"].detach().cpu().numpy(),
+                        output["reg"].shape[3],
+                        output["reg"].shape[2],
+                    )
+                ).to(opt.device)
+
+            hm_loss += self.crit(output["hm"], batch["hm"]) / opt.num_stacks
+            if opt.wh_weight > 0:
+                if opt.dense_wh:
+                    mask_weight = batch["dense_wh_mask"].sum() + 1e-4
+                    wh_loss += (
+                        self.crit_wh(
+                            output["wh"] * batch["dense_wh_mask"],
+                            batch["dense_wh"] * batch["dense_wh_mask"],
+                        )
+                        / mask_weight
+                    ) / opt.num_stacks
+                elif opt.cat_spec_wh:
+                    wh_loss += (
+                        self.crit_wh(
+                            output["wh"],
+                            batch["cat_spec_mask"],
+                            batch["ind"],
+                            batch["cat_spec_wh"],
+                        )
+                        / opt.num_stacks
+                    )
+                else:
+                    wh_loss += (
+                        self.crit_reg(
+                            output["wh"], batch["reg_mask"], batch["ind"], batch["wh"]
+                        )
+                        / opt.num_stacks
+                    )
+
+            if opt.reg_offset and opt.off_weight > 0:
+                off_loss += (
+                    self.crit_reg(
+                        output["reg"], batch["reg_mask"], batch["ind"], batch["reg"]
+                    )
+                    / opt.num_stacks
+                )
+
+        loss = (
+            opt.hm_weight * hm_loss
+            + opt.wh_weight * wh_loss
+            + opt.off_weight * off_loss
+        )
+        loss_stats = {
+            "loss": loss,
+            "hm_loss": hm_loss,
+            "wh_loss": wh_loss,
+            "off_loss": off_loss,
+        }
+        return loss, loss_stats
+
+
+def _gather_feat(feat, ind, mask=None):
+    dim  = feat.size(2)
+    ind  = ind.unsqueeze(2).expand(ind.size(0), ind.size(1), dim)
+    feat = feat.gather(1, ind)
+    if mask is not None:
+        mask = mask.unsqueeze(2).expand_as(feat)
+        feat = feat[mask]
+        feat = feat.view(-1, dim)
+    return feat
+
+def _transpose_and_gather_feat(feat, ind):
+    feat = feat.permute(0, 2, 3, 1).contiguous()
+    feat = feat.view(feat.size(0), -1, feat.size(3))
+    feat = _gather_feat(feat, ind)
+    return feat
+
+def _nms(heat, kernel=3):
+    pad = (kernel - 1) // 2
+
+    hmax = nn.functional.max_pool2d(
+        heat, (kernel, kernel), stride=1, padding=pad)
+    keep = (hmax == heat).float()
+    return heat * keep
+
+def _topk(scores, K=40):
+    batch, cat, height, width = scores.size()
+      
+    topk_scores, topk_inds = torch.topk(scores.view(batch, cat, -1), K)
+
+    topk_inds = topk_inds % (height * width)
+    topk_ys   = (topk_inds // width).int().float()
+    topk_xs   = (topk_inds % width).int().float()
+      
+    topk_score, topk_ind = torch.topk(topk_scores.view(batch, -1), K)
+    topk_clses = (topk_ind // K).int()
+    topk_inds = _gather_feat(
+        topk_inds.view(batch, -1, 1), topk_ind).view(batch, K)
+    topk_ys = _gather_feat(topk_ys.view(batch, -1, 1), topk_ind).view(batch, K)
+    topk_xs = _gather_feat(topk_xs.view(batch, -1, 1), topk_ind).view(batch, K)
+
+    return topk_score, topk_inds, topk_clses, topk_ys, topk_xs
+
+def ctdet_decode(heat, wh, reg=None, cat_spec_wh=False, K=100):
+    batch, cat, height, width = heat.size()
+
+    # heat = torch.sigmoid(heat)
+    # perform nms on heatmaps
+    heat = _nms(heat)
+      
+    scores, inds, clses, ys, xs = _topk(heat, K=K)
+    if reg is not None:
+      reg = _transpose_and_gather_feat(reg, inds)
+      reg = reg.view(batch, K, 2)
+      xs = xs.view(batch, K, 1) + reg[:, :, 0:1]
+      ys = ys.view(batch, K, 1) + reg[:, :, 1:2]
+    else:
+      xs = xs.view(batch, K, 1) + 0.5
+      ys = ys.view(batch, K, 1) + 0.5
+    wh = _transpose_and_gather_feat(wh, inds)
+    if cat_spec_wh:
+      wh = wh.view(batch, K, cat, 2)
+      clses_ind = clses.view(batch, K, 1, 1).expand(batch, K, 1, 2).long()
+      wh = wh.gather(2, clses_ind).view(batch, K, 2)
+    else:
+      wh = wh.view(batch, K, 2)
+    clses  = clses.view(batch, K, 1).float()
+    scores = scores.view(batch, K, 1)
+    bboxes = torch.cat([xs - wh[..., 0:1] / 2, 
+                        ys - wh[..., 1:2] / 2,
+                        xs + wh[..., 0:1] / 2, 
+                        ys + wh[..., 1:2] / 2], dim=2)
+    detections = torch.cat([bboxes, scores, clses], dim=2)
+      
+    return detections
+
